4def454ff90b16a2e01d0263e24ba293a52cfb56
Bruna Araujo <bruna0952@hotmail.com>
2023-10-06 15:20:09 -0300

    implementando Aluno 2

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -29,5 +29,29 @@ public class Aluno {
     public void setdata_entrada(String data_entrada){
         this.data_entrada = data_entrada;
     }
+
+    public String getdata_saida(){
+        return data_saida;
+    }
     
+    public void setdata_saida(String data_saida) {
+        this.data_saida = data_saida;
+    }
+
+    public String getstatus(){
+        return status;
+    }
+
+    public void setstatus(String status){
+        this.status = status;
+    }
+
+    public List getnotas(){
+        return notas;
+    }
+
+    public void setnotas( List notas){
+        this.notas = notas;
+    }
+
 }
